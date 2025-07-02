55dbf42bb01e1eb4ff8b9e4825b4a6254f3b4a6c
Kaden Nichols <kadenstryder.pro@gmail.com>
2025-07-02 14:10:18 -0700

    Project Tidying
    Removed empty format folder
    Removed brainstorm docs from public view in repo

--- format/utils/config.py
+++
@@ -1,9 +0,0 @@
-'''
-This file will store project-specific configurations and parameters
-File paths for audio input and output.
-
-Parameters for spectral analysis (e.g., window size, hop length).
-
-Definitions for byte resolution and structure.
-'''
-
